30b3723eb35053906485a7dde070a22e5b1b92c9
Michal <michalgierczak2504@gmail.com>
2018-06-17 14:34:18 +0200

    Split app into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { SkillsItemComponent } from './skills/skills-item/skills-item.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ExperienceItemComponent } from './experience/experience-item/experience-item.component';
+import { EducationComponent } from './education/education.component';
+import { EducationItemComponent } from './education/education-item/education-item.component';
+import { AwardsComponent } from './awards/awards.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    SkillsItemComponent,
+    ExperienceComponent,
+    ExperienceItemComponent,
+    EducationComponent,
+    EducationItemComponent,
+    AwardsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule

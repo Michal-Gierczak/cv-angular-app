50decf0bd94940c117fdedba1387d727aa67b14a
Michal <michalgierczak2504@gmail.com>
2018-06-17 21:37:56 +0200

    Added skills model and finished skills section

--- src/app/skills/skills-item/skills-item.component.ts
+++ src/app/skills/skills-item/skills-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Skills } from '../skills.model';
+
 @Component({
   selector: 'app-skills-item',
   templateUrl: './skills-item.component.html',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsItemComponent implements OnInit {
 
-  constructor() { }
+  skills: Skills[] = [
+    new Skills('Web Development', 'Javascript, Coffeescript, JAVA, Python, PHP, Go, Node.js, MongoDB, Redis, PostgreSQL, WebStorm.'),
+    new Skills("UI\\UX Design", 'Photoshop, Illustrator, Sketch, Axure, XMind, Prototyping, Wireframing, User Research, Usability Testing.'),
+    new Skills("Frontend Development", 'HTML, CSS, LESS, SASS, Bootstrap, Wordpress, Drupal, Phonegap / Cordova, Ionic, Foundation, Angular.js.'),
+    new Skills("Consulting & SEO Audit", 'Screaming Frog, Woorank, Raventools, Semrush, Moz, WebCEO, Google Analytics, ChartBeat, CrazyEgg.')
+  ];
+
+  constructor() {}
 
   ngOnInit() {
   }

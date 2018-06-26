3e358f6d0fdf5068aa789c128005688c35652b4f
Michal <michalgierczak2504@gmail.com>
2018-06-26 17:00:48 +0200

    Added general experience section description

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -11,12 +11,18 @@ import { ExperienceService } from './experience.service';
 export class ExperienceComponent implements OnInit {
 
   experiences: Experience[] = [];
+  expDesc: string = "";
 
   constructor(private experienceService: ExperienceService) { }
 
   ngOnInit() {
     this.experienceService.getExperiences().subscribe(experiences => {
       this.experiences = experiences;
+      for(let experience of this.experiences) {
+        if (experience['expDesc']) {
+          this.expDesc = experience.expDesc;
+        }
+      }
     })
   }
 
--- src/app/experience/experience.model.ts
+++ src/app/experience/experience.model.ts
@@ -4,12 +4,14 @@ export class Experience {
   public date: string;
   public desc: string;
   public image: string;
+  public expDesc?: string;
 
-  constructor(position: string, company:string, startDate: string, desc: string, image: string) {
+  constructor(position: string, company:string, startDate: string, desc: string, image: string, expDesc?:string) {
     this.position = position;
     this.company = company;
     this.date = startDate;
     this.image = image;
     this.desc = desc;
+    this.expDesc = expDesc;
   }
 }

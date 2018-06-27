a8ee85a7357bc961fea658721e2c6983d2077a43
Michal <michalgierczak2504@gmail.com>
2018-06-27 23:53:47 +0200

    Added getting doc.id from firestore to section-description-service

--- src/app/shared/section-description.service.ts
+++ src/app/shared/section-description.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
+
 export class SectionDescriptionService {
 
-  sectionDescriptionColletion:AngularFirestoreCollection<{}>;
-  sectionDescriptions: Observable<{}>;
+  sectionDescriptionColletion:AngularFirestoreCollection<SectionDesc>;
+  sectionDescriptions: Observable<SectionDesc[]>;
 
   constructor(public afs: AngularFirestore) {
-    this.sectionDescriptions = this.afs.collection('sectionsDescription').valueChanges();
+    //this.sectionDescriptions = this.afs.collection('sectionsDescription').valueChanges();
+    this.sectionDescriptions = this.afs.collection('sectionsDescription').snapshotChanges().pipe(map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as SectionDesc
+        data.id = a.payload.doc.id;
+        return data;
+      });
+  }));
   }
 
   getSectionDescription() {
     return this.sectionDescriptions;
   }
+}
+interface SectionDesc {
+    id?: string;
+    experiences: string;
+    awards: string;
+    skills: string;
+    educations: string;
 }
